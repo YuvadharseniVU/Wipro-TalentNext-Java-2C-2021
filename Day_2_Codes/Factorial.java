7964f4c32bb4b5b723a5f2942b96760358ceb1b2
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-27 19:49:27 +0530

    Update Factorial.java
    Java Program for factorial of a given number

--- Day_2_Codes/Factorial.java
+++ Day_2_Codes/Factorial.java
@@ -2,30 +2,16 @@
 
 import java.util.*;
 
-
 public class Factorial {
-
-    public static void main(String[] args) {
-
-	int num;
-
-        Scanner sc= new Scanner(System.in);
-
-        System.out.println("Enter the value of N");
-
-	num= sc.nextInt();
-
-	long factorial = 1;
-
-	for(int i = 1; i <= num; ++i)
- {
-
-            factorial *= i;
-        
+	public static void main(String[] args) {
+		int num;
+		Scanner sc= new Scanner(System.in);		//get input from user
+		System.out.println("Enter the value of N");
+		num= sc.nextInt();
+		long factorial = 1;
+		for(int i = 1; i <= num; ++i) {
+			factorial *= i;
+		}
+		System.out.printf("Factorial of %d = %d", num, factorial);
 	}
-	
-System.out.printf("Factorial of %d = %d", num, factorial);
-
-    }
-
 }
