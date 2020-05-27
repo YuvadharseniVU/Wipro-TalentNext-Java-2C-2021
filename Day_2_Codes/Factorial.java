f0e3f3ddfd6d407a0ec34677ed8b9c32b9f72a96
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-27 19:46:04 +0530

    Add files via upload

---
+++ Day_2_Codes/Factorial.java
@@ -0,0 +1,31 @@
+	//Factorial of a given number
+
+import java.util.*;
+
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+	int num;
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the value of N");
+
+	num= sc.nextInt();
+
+	long factorial = 1;
+
+	for(int i = 1; i <= num; ++i)
+ {
+
+            factorial *= i;
+        
+	}
+	
+System.out.printf("Factorial of %d = %d", num, factorial);
+
+    }
+
+}
