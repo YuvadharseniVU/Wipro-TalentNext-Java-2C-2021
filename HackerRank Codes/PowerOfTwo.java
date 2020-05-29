9c7a49344228d3fac04b02698532246961f256e4
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-29 21:58:49 +0530

    Add files via upload

---
+++ HackerRank Codes/PowerOfTwo.java
@@ -0,0 +1,27 @@
+	//Power of Two
+	// Date :29-05-2020
+
+class Result {
+	public static List<Integer> isPower(List<Integer> arr) {
+        	ArrayList<Integer> newList = new ArrayList();
+        	ArrayList<Integer> nList = new ArrayList(); 
+        	for(int i:arr){
+            		if(!newList.contains(i))
+               			newList.add(i); 
+        	}
+        	for(int j:arr)
+            		if(isPowerOfTwo(j))
+                		nList.add(1);
+            		else
+                		nList.add(0);
+
+    		return(nList);
+    	}
+    	static boolean isPowerOfTwo(int n) { 
+    		if(n==0) 
+    			return false; 
+  
+	return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==  
+       		(int)(Math.floor(((Math.log(n) / Math.log(2))))); 
+	} 
+}
