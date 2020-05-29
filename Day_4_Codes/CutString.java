2251febf8ce819e47d63354802893df0acc3b6e1
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-29 21:22:35 +0530

    Add files via upload

---
+++ Day_4_Codes/CutString.java
@@ -0,0 +1,12 @@
+	/*Given a string, return a version without the first and last char, so "Wipro" yields "ipr". The string length will be at least 2.*/
+
+import java.util.*;
+
+public class CutString {
+
+	public static void main(String[] args) {
+		String str = "Wipro";		
+		str = str.substring(1, str.length() - 1);		
+		System.out.println(str);
+	}
+}
