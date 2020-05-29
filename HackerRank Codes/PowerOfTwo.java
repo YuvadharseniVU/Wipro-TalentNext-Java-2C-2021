814b4e155221bdd29accab9a025f0ff2bbe742cf
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-29 21:59:36 +0530

    Update PowerOfTwo.java

--- HackerRank Codes/PowerOfTwo.java
+++ HackerRank Codes/PowerOfTwo.java
@@ -21,7 +21,7 @@ class Result {
     		if(n==0) 
     			return false; 
   
-	return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==  
-       		(int)(Math.floor(((Math.log(n) / Math.log(2))))); 
+		return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==  
+       			(int)(Math.floor(((Math.log(n) / Math.log(2))))); 
 	} 
 }
