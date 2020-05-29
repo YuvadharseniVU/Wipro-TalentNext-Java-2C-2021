6303460b62c2dc78dae2ffe484f61b9840f391c3
Yuvadharseni V U <65942657+YuvadharseniVU@users.noreply.github.com>
2020-05-29 21:23:24 +0530

    Update CutString.java

--- Day_4_Codes/CutString.java
+++ Day_4_Codes/CutString.java
@@ -1,9 +1,8 @@
-	/*Given a string, return a version without the first and last char, so "Wipro" yields "ipr". The string length will be at least 2.*/
+/*Given a string, return a version without the first and last char, so "Wipro" yields "ipr". The string length will be at least 2.*/
 
 import java.util.*;
 
 public class CutString {
-
 	public static void main(String[] args) {
 		String str = "Wipro";		
 		str = str.substring(1, str.length() - 1);		
